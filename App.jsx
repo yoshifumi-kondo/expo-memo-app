317bd63badb34f22eb57cd41038e11bef1a3acee
yoshifumi-kondo <kondoy@moonbase.tokyo>
2022-06-10 16:25:24 +0900

    init comit

--- App.jsx
+++ App.jsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line react/style-prop-object */}
       <StatusBar style="auto" />
     </View>
   );
